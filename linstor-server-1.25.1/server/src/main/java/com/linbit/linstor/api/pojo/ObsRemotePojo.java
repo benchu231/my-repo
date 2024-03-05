f82aa6f989921797b12b2d0c4809daeb3354214f
benchu231 <134594125+benchu231@users.noreply.github.com>
2024-03-05 16:38:27 +0800

    Add files via upload

--- linstor-server-1.25.1/server/src/main/java/com/linbit/linstor/api/pojo/ObsRemotePojo.java
+++ linstor-server-1.25.1/server/src/main/java/com/linbit/linstor/api/pojo/ObsRemotePojo.java
@@ -1,1 +1,98 @@
+package com.linbit.linstor.api.pojo;
 
+import java.util.UUID;
+
+public class ObsRemotePojo implements Comparable<ObsRemotePojo>
+{
+    private final UUID uuid;
+    private final String remoteName;
+    private final long flags;
+    private final String endpoint;
+    private final String bucket;
+    private final String region;
+    private final byte[] accessKey;
+    private final byte[] secretKey;
+    private final Long fullSyncId;
+    private final Long updateId;
+
+    public ObsRemotePojo(
+            UUID uuidRef,
+            String remoteNameRef,
+            long flagsRef,
+            String endpointRef,
+            String bucketRef,
+            String regionRef,
+            byte[] accessKeyRef,
+            byte[] secretKeyRef,
+            Long fullSyncIdRef,
+            Long updateIdRef
+    )
+    {
+        uuid = uuidRef;
+        remoteName = remoteNameRef;
+        flags = flagsRef;
+        endpoint = endpointRef;
+        bucket = bucketRef;
+        region = regionRef;
+        accessKey = accessKeyRef;
+        secretKey = secretKeyRef;
+        fullSyncId = fullSyncIdRef;
+        updateId = updateIdRef;
+    }
+
+    public UUID getUuid()
+    {
+        return uuid;
+    }
+
+    public String getRemoteName()
+    {
+        return remoteName;
+    }
+
+    public long getFlags()
+    {
+        return flags;
+    }
+
+    public String getEndpoint()
+    {
+        return endpoint;
+    }
+
+    public String getBucket()
+    {
+        return bucket;
+    }
+
+    public String getRegion()
+    {
+        return region;
+    }
+
+    public byte[] getAccessKey()
+    {
+        return accessKey;
+    }
+
+    public byte[] getSecretKey()
+    {
+        return secretKey;
+    }
+
+    public Long getFullSyncId()
+    {
+        return fullSyncId;
+    }
+
+    public Long getUpdateId()
+    {
+        return updateId;
+    }
+
+    @Override
+    public int compareTo(ObsRemotePojo otherObsRemote)
+    {
+        return remoteName.compareTo(otherObsRemote.remoteName);
+    }
+}
