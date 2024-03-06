ca2640c752f0fbf3e40cea536885a7c386f6e9ec
benchu231 <134594125+benchu231@users.noreply.github.com>
2024-03-06 19:35:43 +0800

    Add files via upload

---
+++ linstor-server-1.25.1/satellite/src/main/java/com/linbit/linstor/api/protobuf/ApplyRemote.java
@@ -0,0 +1,156 @@
+package com.linbit.linstor.api.protobuf;
+
+import com.linbit.ImplementationError;
+import com.linbit.linstor.InternalApiConsts;
+import com.linbit.linstor.api.ApiCall;
+import com.linbit.linstor.api.pojo.EbsRemotePojo;
+import com.linbit.linstor.api.pojo.ObsRemotePojo;
+import com.linbit.linstor.api.pojo.S3RemotePojo;
+import com.linbit.linstor.api.pojo.StltRemotePojo;
+import com.linbit.linstor.core.apicallhandler.StltApiCallHandler;
+import com.linbit.linstor.logging.ErrorReporter;
+import com.linbit.linstor.proto.javainternal.c2s.IntEbsRemoteOuterClass.IntEbsRemote;
+import com.linbit.linstor.proto.javainternal.c2s.IntObsRemoteOuterClass.IntObsRemote;
+import com.linbit.linstor.proto.javainternal.c2s.IntS3RemoteOuterClass.IntS3Remote;
+import com.linbit.linstor.proto.javainternal.c2s.IntStltRemoteOuterClass.IntStltRemote;
+import com.linbit.linstor.proto.javainternal.c2s.MsgIntApplyRemoteOuterClass.MsgIntApplyRemote;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.TreeMap;
+import java.util.UUID;
+
+@ProtobufApiCall(
+    name = InternalApiConsts.API_APPLY_REMOTE,
+    description = "Registeres a remote on this satellite"
+)
+@Singleton
+public class ApplyRemote implements ApiCall
+{
+    private final StltApiCallHandler apiCallHandler;
+    private final ErrorReporter errorReporter;
+
+    @Inject
+    public ApplyRemote(
+        StltApiCallHandler apiCallHandlerRef,
+        ErrorReporter errorReporterRef
+    )
+    {
+        apiCallHandler = apiCallHandlerRef;
+        errorReporter = errorReporterRef;
+    }
+
+    @Override
+    public void execute(InputStream msgDataIn)
+        throws IOException
+    {
+        MsgIntApplyRemote msgApplyRemote = MsgIntApplyRemote.parseDelimitedFrom(msgDataIn);
+
+        if (msgApplyRemote.hasS3Remote())
+        {
+            S3RemotePojo s3remotePojo = asS3RemotePojo(
+                msgApplyRemote.getS3Remote(),
+                msgApplyRemote.getFullSyncId(),
+                msgApplyRemote.getUpdateId()
+            );
+            apiCallHandler.applyS3RemoteChanges(s3remotePojo);
+        }
+        else if (msgApplyRemote.hasObsRemote())
+        {
+            ObsRemotePojo obsRemotePojo = asObsRemotePojo(
+                msgApplyRemote.getObsRemote(),
+                msgApplyRemote.getFullSyncId(),
+                msgApplyRemote.getUpdateId()
+            );
+            apiCallHandler.applyObsRemoteChanges(obsRemotePojo);
+        }
+        else if (msgApplyRemote.hasSatelliteRemote())
+        {
+            StltRemotePojo stltRemotePojo = asStltRemotePojo(
+                msgApplyRemote.getSatelliteRemote(),
+                msgApplyRemote.getFullSyncId(),
+                msgApplyRemote.getUpdateId()
+            );
+            apiCallHandler.applyStltRemoteChanges(stltRemotePojo);
+        }
+        else if (msgApplyRemote.hasEbsRemote())
+        {
+            EbsRemotePojo ebsRemotePojo = asEbsRemotePojo(
+                msgApplyRemote.getEbsRemote(),
+                msgApplyRemote.getFullSyncId(),
+                msgApplyRemote.getUpdateId()
+            );
+            apiCallHandler.applyEbsRemoteChanges(ebsRemotePojo);
+        }
+        else
+        {
+            errorReporter.reportError(new ImplementationError("Message does not have a known remote type."));
+        }
+
+    }
+
+    static S3RemotePojo asS3RemotePojo(IntS3Remote proto, long fullSyncId, long updateId)
+    {
+        return new S3RemotePojo(
+            UUID.fromString(proto.getUuid()),
+            proto.getName(),
+            proto.getFlags(),
+            proto.getEndpoint(),
+            proto.getBucket(),
+            proto.getRegion(),
+            proto.getAccessKey().toByteArray(),
+            proto.getSecretKey().toByteArray(),
+            fullSyncId,
+            updateId
+        );
+    }
+
+
+    static ObsRemotePojo asObsRemotePojo(IntObsRemote proto, long fullSyncId, long updateId)
+    {
+        return new ObsRemotePojo(
+            UUID.fromString(proto.getUuid()),
+            proto.getName(),
+            proto.getFlags(),
+            proto.getEndpoint(),
+            proto.getBucket(),
+            proto.getRegion(),
+            proto.getAccessKey().toByteArray(),
+            proto.getSecretKey().toByteArray(),
+            fullSyncId,
+            updateId
+        );
+    }
+
+    static EbsRemotePojo asEbsRemotePojo(IntEbsRemote proto, long fullSyncId, long updateId)
+    {
+        return new EbsRemotePojo(
+            UUID.fromString(proto.getUuid()),
+            proto.getName(),
+            proto.getFlags(),
+            proto.getUrl(),
+            proto.getAvailabilityZone(),
+            proto.getRegion(),
+            proto.getAccessKey().toByteArray(),
+            proto.getSecretKey().toByteArray(),
+            fullSyncId,
+            updateId
+        );
+    }
+
+    static StltRemotePojo asStltRemotePojo(IntStltRemote proto, long fullSyncId, long updateId)
+    {
+        return new StltRemotePojo(
+            UUID.fromString(proto.getUuid()),
+            proto.getName(),
+            proto.getFlags(),
+            proto.getTargetIp(),
+            new TreeMap<>(proto.getTargetPortsMap()),
+            proto.getUseZstd(),
+            fullSyncId,
+            updateId
+        );
+    }
+}
