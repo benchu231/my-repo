8845b241ad30e3940621c7a92461768ba4e28911
benchu231 <134594125+benchu231@users.noreply.github.com>
2024-03-06 16:17:49 +0800

    Add files via upload

--- linstor-server-1.25.1/controller/src/main/java/com/linbit/linstor/core/repository/RemoteRepository.java
+++ linstor-server-1.25.1/controller/src/main/java/com/linbit/linstor/core/repository/RemoteRepository.java
@@ -1,1 +1,57 @@
+package com.linbit.linstor.core.repository;
 
+import com.linbit.linstor.api.ApiCallRcImpl;
+import com.linbit.linstor.api.ApiConsts;
+import com.linbit.linstor.core.CoreModule;
+import com.linbit.linstor.core.apicallhandler.response.ApiRcException;
+import com.linbit.linstor.core.identifier.RemoteName;
+import com.linbit.linstor.core.objects.remotes.AbsRemote;
+import com.linbit.linstor.core.objects.remotes.ObsRemote;
+import com.linbit.linstor.core.objects.remotes.S3Remote;
+import com.linbit.linstor.security.AccessContext;
+import com.linbit.linstor.security.AccessDeniedException;
+import com.linbit.linstor.security.AccessType;
+import com.linbit.linstor.security.ProtectedObject;
+
+public interface RemoteRepository extends ProtectedObject
+{
+    void requireAccess(AccessContext accCtx, AccessType requested) throws AccessDeniedException;
+
+    AbsRemote get(AccessContext accCtx, RemoteName remoteName) throws AccessDeniedException;
+
+    void put(AccessContext accCtx, AbsRemote remote) throws AccessDeniedException;
+
+    void remove(AccessContext accCtx, RemoteName remoteName) throws AccessDeniedException;
+
+    CoreModule.RemoteMap getMapForView(AccessContext accCtx) throws AccessDeniedException;
+
+    default S3Remote getS3(AccessContext accCtx, RemoteName remoteName) throws AccessDeniedException
+    {
+        AbsRemote remote = get(accCtx, remoteName);
+        if (remote != null && !(remote instanceof S3Remote))
+        {
+            throw new ApiRcException(
+                ApiCallRcImpl.simpleEntry(
+                    ApiConsts.FAIL_INVLD_REMOTE_NAME,
+                    "Given Remote was not of class S3Remote but of " + remote.getClass().getCanonicalName()
+                )
+            );
+        }
+        return (S3Remote) remote;
+    }
+
+    default ObsRemote getObs(AccessContext accCtx, RemoteName remoteName) throws AccessDeniedException
+    {
+        AbsRemote remote = get(accCtx, remoteName);
+        if (remote != null && !(remote instanceof ObsRemote))
+        {
+            throw new ApiRcException(
+                ApiCallRcImpl.simpleEntry(
+                    ApiConsts.FAIL_INVLD_REMOTE_NAME,
+                    "Given Remote was not of class ObsRemote but of " + remote.getClass().getCanonicalName()
+                )
+            );
+        }
+        return (ObsRemote) remote;
+    }
+}
